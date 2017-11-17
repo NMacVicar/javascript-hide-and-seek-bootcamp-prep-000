2f05796a0162a440b6c8d46543b0e8df0c24f15d
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-17 21:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  let rankedLists = document.querySelectorAll('.ranked-list')
-   
+  let rankings = document.getElementById("app").querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < rankings.length; i++){
+    rankings[i].textContent = parseInt(rankings[i].textContent) + 1
+  }
+
 }
 
 function deepestChild(){
