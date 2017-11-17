ffce97c6198c1e72c8fa9dccf92ef4d5b9c32402
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-17 21:08:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ function nestedTarget(){
 function increaseRankBy(n){
   let rankings = document.getElementById("app").querySelectorAll('ul.ranked-list li')
   for (var i = 0; i < rankings.length; i++){
-    rankings[i].textContent = parseInt(rankings[i].textContent) + 1
+    rankings[i].textContent = (parseInt(rankings[i].textContent) + n).toString()
   }
-
 }
 
 function deepestChild(){
